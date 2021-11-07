273cc980df8792edd5ae0995d46fcdec17a64fa3
Shane Parker <shane.tim.parker@ea.com>
2021-11-07 10:37:44 -0500

    Added support for +PB to AC

--- scripts/manager_actor_fz.lua
+++ scripts/manager_actor_fz.lua
@@ -1,8 +1,12 @@
 local getActorRecordTypeFromPathOriginal;
+local getDefenseValueOriginal;
 
 function onInit()
 	getActorRecordTypeFromPathOriginal = ActorManager.getActorRecordTypeFromPath;
 	ActorManager.getActorRecordTypeFromPath = getActorRecordTypeFromPath;
+	
+	getDefenseValueOriginal = ActorManager5E.getDefenseValue;
+	ActorManager5E.getDefenseValue = getDefenseValue;
 end
 
 function getActorRecordTypeFromPath(sActorNodePath)
@@ -15,4 +19,25 @@ function getActorRecordTypeFromPath(sActorNodePath)
 		end
 	end
 	return result;
+end
+
+function getDefenseValue(rAttacker, rDefender, rRoll)
+	local nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, bADV, bDIS = getDefenseValueOriginal(rAttacker, rDefender, rRoll);
+
+	if FriendZone.isCohort(rDefender) then
+		local sAcText = DB.getValue(ActorManager.getCreatureNode(rDefender), "actext", "");
+		if sAcText:gmatch("+ ?PB") then
+			local nodeCommander = FriendZone.getCommanderNode(rDefender);
+			local nProfBonus = DB.getValue(nodeCommander, "profbonus", 0);
+			if nProfbonus == 0 then
+				local sCR = DB.getValue(nodeCommander, "cr");
+				if StringManager.isNumber(sCR) then
+					nProfBonus = math.max(2, math.floor((tonumber(sCR) - 1) / 4) + 2);
+				end
+			end
+			nDefenseVal = nDefenseVal + nProfBonus;
+		end
+	end
+
+	return nDefenseVal, nAtkEffectsBonus, nDefEffectsBonus, bADV, bDIS;
 end
