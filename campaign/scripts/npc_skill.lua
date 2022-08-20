ead73fa7f1a1bc39acd90a447aec694e930207eb
MeAndUnique <shane.tim.parker@gmail.com>
2022-08-20 08:40:34 -0400

    General cohort parsing improvements

--- campaign/scripts/npc_skill.lua
+++ campaign/scripts/npc_skill.lua
@@ -17,11 +17,15 @@ function action(draginfo)
 	local nodeNPC = window.getDatabaseNode();
 	if FriendZone.isCohort(nodeNPC) and nSelection and nSelection ~=0 then
 		local sSelected = getValue():sub(getCursorPosition(), nSelection);
-		local sMatch = sSelected:match("%+ ?PB")
+		local sMatch, sMultipliplier = sSelected:match("%+ ?(PB)[x%*]?(%d*)")
 		if sMatch then
+			local nMultiplier = tonumber(sMultipliplier);
+			if not nMultiplier then
+				nMultiplier = 1;
+			end
 			local nodeCommander = FriendZone.getCommanderNode(nodeNPC);
 			local nProfBonus = DB.getValue(nodeCommander, "profbonus", 0);
-			ActionSkillFZ.setCommanderProfBonus(nProfBonus);
+			ActionSkillFZ.setCommanderProfBonus(nProfBonus * nMultiplier);
 		end
 	end
 	actionOriginal(draginfo)
